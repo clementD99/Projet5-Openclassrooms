3bf6b4b53f26cfdfdd2f2b2c67cdacb66d24218a
Clément Desmur <clement.desmur@gmail.com>
2024-02-27 16:32:10 +0100

    correction des images

--- script.js
+++ script.js
@@ -18,19 +18,56 @@ const slides = [
   },
 ];
 
-let currentindex = 0;
+let currentIndex = 0;
+const banner = document.getElementById("banner");
+const tagLine = document.getElementById("tagLine");
+const image = document.querySelector(".banner-img");
+const dot = document.querySelectorAll('.dot_selected');
 
-function updateCarrousel() {
-  carrousel.style.trasnform = "translateX(${-currentIndex * 100}%)";
+
+// flèches gauche et droite 
+let arrow_left = document.getElementById("prevSlide");
+let arrow_right = document.getElementById("nextSlide");
+// fin flèches gauche et droite 
+
+
+// permet de cliquer sur les flèches 
+arrow_left.addEventListener("click", () => {
+  console.log("prevSlide");
+  prevSlide();
+  console.log(currentIndex);
+});
+  
+arrow_right.addEventListener("click", () => {
+  console.log("nextSlide");
+  nextSlide();
+  console.log(currentIndex);
+});
+// fin permet de cliquer sur les flèches
+
+
+// pour faire défiler les images
+function updatecarousel() {
+  image.src = `./images/${slides[currentIndex].image}`;
+  tagLine.innerHTML = slides[currentIndex].tagLine;
 }
+// fin pour faire défiler les images
+
 
 function nextSlide() {
-  currentIndex = (currentIndex + 1) % carousel.children.length;
-  updateCarrousel();
+  currentIndex = (currentIndex + 1) % slides.length;
+  updatecarousel();
 }
 
 function prevSlide() {
-  currentIndex =
-    (currentIndex - 1 + carousel.children.length) % carousel.children.length;
-  updateCarrousel();
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updatecarousel();
+}
+
+function createDots() {
+  for (const slide of slides) {
+    const dot = document.createElement("span");
+    dot.className = "dot";
+    dot.appendChild(dot);
+  }
 }
